8f4c3db8b0c97d59e184b1b32d1caf15550319b4
Fernando Hernandez <ferjavi6664@gmail.com>
2026-03-16 16:13:35 -0500

    test route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;;
+Route::get('/', function () {
+    return "SINGLA funcionando en Railway 🚀";
+});
+
 Route::get('/', [DashboardController::class, 'inicio']);
 Route::get('/reportes', [DashboardController::class, 'reportes']);
 Route::get('/usuarios', [DashboardController::class, 'usuarios']);
